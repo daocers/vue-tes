a7d569727d1f9ffc69322759cd38cf32d134f0a0
leiyang <lei890627>
2017-11-06 20:47:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<div id="app5" class="demo">
+  <input v-model="newTodo" v-on:keyup.enter="addTodo">
+  <ul>
+    <li v-for="todo in todos">
+      <span>{{ todo.text }}</span>
+      <button v-on:click="removeTodo($index)">X</button>
+    </li>
+  </ul>
+</div>
+<script>
+  new Vue({
+    el: '#app5',
+    data: {
+      newTodo: '',
+      todos: [
+        { text: 'Add some todos' }
+      ]
+    },
+    methods: {
+      addTodo: function () {
+        var text = this.newTodo.trim()
+        if (text) {
+          this.todos.push({ text: text })
+          this.newTodo = ''
+        }
+      },
+      removeTodo: function (index) {
+        this.todos.splice(index, 1)
+      }
+    }
+  })
+</script>
---
+++ src/components/tes-nav.vue
@@ -0,0 +1,11 @@
+<template>
+  <el-meun theme="dark" default-active="1" class="ele-menu-demo" mode="horizontal" :router="true">
+    <el-menu-item index="/elindex/hot">处理中心</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">我的工作台</template>
+      <el-menu-item index="2-1">选项1</el-menu-item>
+      <el-menu-item index="2-2">选项2</el-menu-item>
+      <el-menu-item index="2-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-meun>
+</template>
