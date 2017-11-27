4762632e987f9206a9fbdaf3a0327cddb54a27b8
leiyang <lei890627>
2017-11-28 00:25:37 +0800

    提交

--- src/page/commonQuestion/list.vue
+++ src/page/commonQuestion/list.vue
@@ -10,6 +10,7 @@
       </el-form-item>
       <el-form-item style="float: right">
         <el-button type="primary" @click="toAdd()">添加</el-button>
+        <el-button type="primary" @click="toBatchAdd()">批量添加</el-button>
       </el-form-item>
     </el-form>
 
@@ -162,15 +163,15 @@
         <el-form-item label="题目" prop="title">
           <el-input v-model="dataForAdd.title" placeholder="请输入"></el-input>
         </el-form-item>
-        <el-form-item label="答案" v-show="dataForAdd.questionTypeId != null" prop="answer">
+        <el-form-item label="答案" v-show="dataForAdd.questionTypeId != null" prop="answerItemList">
           <div v-if="questionTypeMap[dataForAdd.questionTypeId] == 'single'">
-            <el-radio v-model="dataForAdd.answer" label="A" border size="medium">A</el-radio>
-            <el-radio v-model="dataForAdd.answer" label="B" border size="medium">B</el-radio>
-            <el-radio v-model="dataForAdd.answer" label="C" border size="medium">C</el-radio>
-            <el-radio v-model="dataForAdd.answer" label="D" border size="medium">D</el-radio>
+            <el-radio v-model="dataForAdd.answerItemList" label="A" border size="medium">A</el-radio>
+            <el-radio v-model="dataForAdd.answerItemList" label="B" border size="medium">B</el-radio>
+            <el-radio v-model="dataForAdd.answerItemList" label="C" border size="medium">C</el-radio>
+            <el-radio v-model="dataForAdd.answerItemList" label="D" border size="medium">D</el-radio>
           </div>
           <div v-else-if="questionTypeMap[dataForAdd.questionTypeId] == 'multi'">
-            <el-checkbox-group v-model="dataForAdd.answerList" @change="handleCheckBox">
+            <el-checkbox-group v-model="dataForAdd.answerItemList" @change="handleCheckBox">
               <el-checkbox label="A">A</el-checkbox>
               <el-checkbox label="B">B</el-checkbox>
               <el-checkbox label="C">C</el-checkbox>
@@ -179,16 +180,16 @@
             </el-checkbox-group>
           </div>
           <div v-else-if="questionTypeMap[dataForAdd.questionTypeId] == 'judge'">
-            <el-radio v-model="dataForAdd.answer" label="T" border size="medium">正确</el-radio>
-            <el-radio v-model="dataForAdd.answer" label="F" border size="medium">错误</el-radio>
+            <el-radio v-model="dataForAdd.answerItemList" label="T" border size="medium">正确</el-radio>
+            <el-radio v-model="dataForAdd.answerItemList" label="F" border size="medium">错误</el-radio>
           </div>
           <!--<el-input v-model="dataForAdd.answer" placeholder="请输入"></el-input>-->
         </el-form-item>
-        <el-form-item label="内容" v-show="questionTypeMap[dataForAdd.questionTypeId] != 'judge'" prop="content">
+        <el-form-item label="内容" v-show="questionTypeMap[dataForAdd.questionTypeId] != 'judge'" prop="contentItemList">
           <!--<div v-for="line in dataForAdd.contentList">-->
-            <!--<el-input v-model="line" placeholder="请输入"></el-input>-->
+          <!--<el-input v-model="line" placeholder="请输入"></el-input>-->
           <!--</div>-->
-          <el-table :data="dataForAdd.contentList"
+          <el-table :data="dataForAdd.contentItemList"
                     border
                     style="width: 100%">
             <el-table-column prop="no" label="序号" width="60px">
@@ -217,7 +218,24 @@
         </el-form-item>
 
         <el-form-item label="属性" prop="propertyItemInfo">
-          <el-input v-model="dataForAdd.propertyItemInfo" placeholder="请输入"></el-input>
+          <!--<el-input v-model="dataForAdd.propertyItemInfo" placeholder="请输入"></el-input>-->
+
+          <div v-for="questionType in questionTypeList">
+            <div v-if="dataForAdd.questionTypeId == questionType.id">
+              <div v-for="property in questionType.propertyList">
+                <el-checkbox-group
+                  v-model="dataForAdd.propertyItemList"
+                  :min="1"
+                  :max="1">
+                  <el-checkbox v-for="itme in property.propertyItemList" :label="item.name" :key="item.id">
+                    {{item.name}}
+                  </el-checkbox>
+                </el-checkbox-group>
+              </div>
+
+            </div>
+
+          </div>
         </el-form-item>
       </el-form>
 
@@ -225,6 +243,58 @@
         <el-button @click="cancelAdd()">取 消</el-button>
         <el-button type="primary" @click="addData()">确 定</el-button>
       </div>
+    </el-dialog>
+
+    <el-dialog title="批量添加" :visible.sync="batchAddDialogShow" :before-close="handleClose">
+      <el-form label-position="left" ref="batchForm" :rules="batchAddRules" :model="dataForBatch" label-width="60px">
+        <el-form-item label="题库" prop="questionBankId">
+          <el-select v-model="dataForBatch.questionBankId" placeholder="请选择题库">
+            <el-option v-for="bank in questionBankList"
+                       :key="bank.id"
+                       :label="bank.name"
+                       :value="bank.id">
+
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="题型" prop="questionTypeId">
+          <el-select v-model="dataForBatch.questionTypeId" placeholder="请选择题型">
+            <el-option
+              v-for="type in questionTypeList"
+              :key="type.id"
+              :label="type.name"
+              :value="type.id">
+
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <el-upload
+        class="upload-demo"
+        ref="upload"
+        :limit="1"
+        action="http://localhost:8090/commonQuestion/api/batchAdd.do"
+        :on-preview="handlePreview"
+        :on-remove="handleRemove"
+        :on-change="handleChange"
+        :on-success="handleSuccess"
+        :on-error="handleError"
+        :file-list="fileList"
+        :auto-upload="false">
+
+        <el-button slot="trigger" size="small" type="primary" plain>选取文件</el-button>
+        <el-button style="margin-left: 10px;" size="small" type="primary" @click="batchAdd">上传到服务器</el-button>
+        <div style="display: inline-block; margin-left: 20px;">
+          没有模板？<a type="success" href="javascript:download()">下载模板</a>
+          <el-button  size="small" type="success" plain @click="download">下载模板</el-button>
+        </div>
+        <div slot="tip" class="el-upload__tip">只能上传下载的模板文件</div>
+      </el-upload>
+
+      <el-alert v-show="batchAddErrorMessage != ''"
+                v-bind:title='batchAddErrorMessage'
+                type="error">
+      </el-alert>
     </el-dialog>
 
   </div>
@@ -242,11 +312,11 @@
       var checkContentInfo = (rule, value, callback) => {
         console.log("value: ", value);
         let contentList;
-        if(this.addDialogShow){
+        if (this.addDialogShow) {
           contentList = this.dataForAdd.contentList;
-        }else if(this.editDialogShow){
+        } else if (this.editDialogShow) {
           contentList = this.dataForEdit.contentList;
-        }else {
+        } else {
 //          只校验单选多选，其他题型不对内容校验
           callback();
         }
@@ -260,7 +330,7 @@
             callback(new Error("试题选项不能为空"));
           } else if (info.length > 50) {
             callback(new Error("试题选项在50位以内"));
-          }  else {
+          } else {
             callback();
           }
         }
@@ -290,8 +360,9 @@
          * 添加对话框数据
          */
         dataForAdd: {
-          contentList:[],
-          answerList: [],
+          contentItemList: [],
+          answerItemList: [],
+          propertyItemList: [],
         },
 
         /**
@@ -303,12 +374,23 @@
          */
         editDialogShow: false,
 
+        batchAddErrorMessage: '',
+        batchAddDialogShow: false,
+        /**
+         * 批量上传下载模板时候的参数信息
+         */
+        dataForBatch: {
+          questionTypeId: null,
+          questionBankId: null,
+        },
+
         /**
          * 修改对话框数据
          */
         dataForEdit: {
-          contentList:[],
-          answerList: [],
+          contentItemList: [],
+          answerItemList: [],
+          propertyItemList: [],
         },
 
         /**
@@ -345,17 +427,17 @@
               {required: true, message: '请输入题目', trigger: 'blur'},
               {max: 100, message: '长度在3-100个字符', trigger: 'blur'}
             ],
-          answer:
+          answerList:
             [
-              {required: true, message: '请输入answer', trigger: 'blur'},
+              {type: 'array', required: true, message: '请输入answer', trigger: 'change'},
 //              {min: 3, max: 10, message: '长度在3-10个字符', trigger: 'blur'}
-            ],
+          ],
           content:
             [
               {validator: checkContentInfo, trigger: 'blur'},
 //              {required: true, message: '请输入content', trigger: 'blur'},
 //              {min: 3, max: 100, message: '长度在3-100个字符', trigger: 'blur'}
-            ],
+          ],
           extraInfo:
             [
               {required: true, message: '请输入extraInfo', trigger: 'blur'},
@@ -363,11 +445,11 @@
             ],
           questionTypeId:
             [
-              {required: true, message: '请输入questionTypeId', trigger: 'blur'},
+              {type: 'number', required: true, message: '请输入questionTypeId', trigger: 'change'},
             ],
           questionBankId:
             [
-              {required: true, message: '请输入questionBankId', trigger: 'blur'},
+              {type: 'number', required: true, message: '请输入questionBankId', trigger: 'change'},
             ],
           propertyItemInfo:
             [
@@ -377,7 +459,19 @@
             [
               {required: true, message: '请输入privaryType', trigger: 'blur'},
 //              {min: 3, max: 10, message: '长度在3-10个字符', trigger: 'blur'}
-            ],
+          ],
+        },
+
+        /**
+         * 上传的校验
+         */
+        batchAddRules:{
+          questionTypeId:[
+            {type: 'number', required: true, message: '请选择题型', trigger: 'change'}
+          ],
+          questionBankId:[
+            {type: 'number', required: true, message: '请选择题库', trigger: 'change'}
+          ]
         }
 
       }
@@ -385,6 +479,37 @@
 
 
     methods: {
+      download(){
+        if(this.dataForBatch.questionTypeId == null){
+          this.batchAddErrorMessage = "请选择题型";
+        }else{
+          this.batchAddErrorMessage = '';
+        }
+        window.location.href = "http://localhost:8090/branch/downloadModel.do";
+      },
+
+      toBatchAdd() {
+        console.log("唤起批量导入对话框。。。")
+//        this.$refs.upload.clearFiles();
+        this.batchAddErrorMessage = '';
+        this.batchAddDialogShow = true;
+      },
+      /**
+       * 批量导入
+       */
+      async batchAdd() {
+        this.$refs['batchForm'].validate(async (valid) => {
+          if (!valid) {
+            console.log("参数校验不通过，请处理");
+            return false;
+          } else {
+            let res = await this.$refs.upload.submit();
+            console.log("上传结果： ", res);
+          }
+        });
+
+      },
+
       /**
        * 查询
        */
@@ -512,9 +637,9 @@
        */
       addItem: function () {
         if (this.editDialogShow) {
-          this.dataForEdit.contentList.push({});
+          this.dataForEdit.contentItemList.push({});
         } else {
-          this.dataForAdd.contentList.push({});
+          this.dataForAdd.contentItemList.push({});
         }
       },
       /**
@@ -522,14 +647,14 @@
        */
       removeLine: function (idx, row) {
         if (this.editDialogShow) {
-          this.dataForEdit.contentList.splice(idx, 1);
+          this.dataForEdit.contentItemList.splice(idx, 1);
         } else {
-          this.dataForAdd.contentList.splice(idx, 1);
+          this.dataForAdd.contentItemList.splice(idx, 1);
         }
       },
 
 
-      handleCheckBox(value){
+      handleCheckBox(value) {
         console.log("复选框选中的值：", value);
       },
       handleSizeChange(val) {
@@ -542,13 +667,65 @@
         console.log(`当前页: ${val}`);
         this.findByCondition();
       },
-      getItemNo:function (no) {
+      getItemNo: function (no) {
         console.log("no: ", no);
-        let info = {0: 'A', 1: 'B',2:'C', 3: 'D', 4: 'E', 5: 'F'}
+        let info = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F'}
         let res = info[no];
         console.log("res: ", res);
         return res;
-      }
+      },
+
+
+      /**
+       * 批量添加对话框关闭时候执行
+       */
+      handleClose(){
+        this.$refs.upload.clearFiles();
+        this.batchAddDialogShow = false;
+      },
+
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handleChange(file, fileList){
+        console.info("change file: ", file);
+        console.info("change fileList: ", fileList);
+
+      },
+      handlePreview(file) {
+        console.log("导入结果：", file.response);
+      },
+
+      /**
+       * 服务器成功响应并处理后回调
+       */
+      handleSuccess(response, file, fileList){
+        console.log("上传结果：", response)
+        if(response.result == false){
+          this.batchAddErrorMessage = response.message;
+        }else{
+          this.findByCondition();
+          this.$refs.upload.clearFiles();
+          this.batchAddDialogShow = false;
+          this.$notify({
+            title: '成功',
+            message: '批量导入机构信息成功',
+            type: 'success',
+            duration: 0
+          });
+        }
+      },
+      handleError(err, file, fileList){
+        console.log("上传失败，原因：", err);
+        this.$refs.upload.clearFiles();
+        this.batchAddDialogShow = false;
+        this.$notify({
+          title: '失败',
+          message: '批量导入机构信息失败',
+          type: 'error',
+          duration: 0
+        });
+      },
     },
     /**
      * 页面初始化时候执行
@@ -558,8 +735,9 @@
       this.findByCondition();
       let questionTypeList = await this.http("/questionType/api/findAll.do");
       this.questionTypeList = questionTypeList;
-      if(questionTypeList != null){
-        for(let i = 0; i < questionTypeList.length; i++){
+      console.log("题型信息： ", questionTypeList)
+      if (questionTypeList != null) {
+        for (let i = 0; i < questionTypeList.length; i++) {
           let questionType = questionTypeList[i];
           this.questionTypeMap[questionType.id] = questionType.code;
         }
@@ -570,8 +748,7 @@
       let propertyList = await this.http("/property/api/findAll.do");
       this.propertyList = propertyList;
     },
-    computed:{
-    }
+    computed: {}
   }
 </script>
 
